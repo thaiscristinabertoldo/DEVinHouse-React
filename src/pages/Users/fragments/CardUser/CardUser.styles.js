bdb3532cff7c66781d716e69a84540881adadf8f
Thais <thaisbertoldo11@gmail.com>
2021-06-28 21:51:15 -0300

    feat: adiciona page users

---
+++ src/pages/Users/fragments/CardUser/CardUser.styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core'
+
+export const useStyles = makeStyles((theme) => ({
+  about: {
+    marginTop: theme.spacing(2),
+  },
+  button: {
+    display: 'flex',
+    marginTop: theme.spacing(2),
+    justifyContent: 'flex-end',
+  },
+}))
