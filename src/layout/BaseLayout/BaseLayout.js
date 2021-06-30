02b91844e909f7b6d7c0d110f2ad750087aff0bb
thais.bertoldo <thais.bertoldo@softplan.com.br>
2021-06-30 10:24:27 -0300

    feat: adiciona prop alteração mode theme

--- src/layout/BaseLayout/BaseLayout.js
+++ src/layout/BaseLayout/BaseLayout.js
@@ -1,12 +1,12 @@
 import { NavBar } from '../../components'
 import { useStyles } from './BaseLayout.styles'
 
-export const BaseLayout = ({ children }) => {
+export const BaseLayout = ({ children, onToggleTheme }) => {
   const classes = useStyles()
 
   return (
     <>
-      <NavBar />
+      <NavBar onToggleTheme={onToggleTheme} />
       <div className={classes.root}>{children}</div>
     </>
   )
